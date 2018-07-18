222ba99807c0121406e5c332f7c8e1e078945ce5
Cailey Lin <ceielle@uw.edu>
2018-07-18 00:08:20 -0700

    Homepage layout with the basic layout

---
+++ mindful/src/BlogPost.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class BlogPost extends Component {
+    constructor(props){
+      super(props);
+    }
+  
+    render() {
+  
+      //console.log(this.props.match.params);
+  
+      const date = new Date(this.props.date);
+      const title = this.props.title;
+      const content = this.props.content;
+      const emotions = this.props.emotions;
+  
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      let numDate = date.getMonth();
+  
+      return (
+        <div className='container mt-3'>
+            <div className='row sticky-top bg-light pt-3'>
+                <div className='col-sm-1 offset-2'>
+                    <span className='date blog-post-meta text-muted'>{months[numDate]} {date.getDate()}</span>
+                </div>
+                <div className='col-sm-8'>
+                    <h2 className='post-title blog-post-title'>{title}</h2>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-sm-8 offset-3'>
+                    {emotions.map(emotion => 
+                      <span className='emation-tags d-inline-block badge badge-secondary mr-2 hover' key={emotion}>{emotion}</span>
+                    )}
+                    <p className='post-content blog-post-meta'>{content}</p>  
+                </div>
+            </div>
+        </div>
+      );
+    }
+  }
+
+  export default BlogPost;
--- mindful/src/index.js
+++ mindful/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+//import 'bootstrap/dist/css/bootstrap.css'; //bootstrap
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
